17a53892737388617cc95dace21f496e06835da6
m4ci3k2 <m4ci3k2@users.noreply.github.com>
2019-08-12 21:38:56 +0200

    add option aliases

--- src/main/java/com/m2mci/mqttKafkaBridge/CommandLineParser.java
+++ src/main/java/com/m2mci/mqttKafkaBridge/CommandLineParser.java
@@ -12,25 +12,25 @@ public class CommandLineParser {
 	private static final String DEFAULT_BROKER_LIST = "localhost:9092";
 	private static final String DEFAULT_MQTT_SERVER_URI = "tcp://localhost:1883";
 
-	@Option(name="--id", usage="MQTT Client ID")
+	@Option(name="--id", aliases="-i", usage="MQTT Client ID")
 	private String clientId = "mqttKafkaBridge";
 
-	@Option(name="--uri", usage="MQTT Server URI")
+	@Option(name="--uri", aliases="-u", usage="MQTT Server URI")
 	private String serverURI = DEFAULT_MQTT_SERVER_URI;
 
 	@Option(name="--brokerlist", aliases="-b", usage="Broker list (comma-separated)")
 	private String brokerList = DEFAULT_BROKER_LIST;
 	
-	@Option(name="--topics", usage="MQTT topic filters (comma-separated)")
+	@Option(name="--topics", aliases="-t", usage="MQTT topic filters (comma-separated)")
 	private String mqttTopicFilters = ALL_MQTT_TOPICS;
 
-	@Option(name="--send-to", usage="kafka topic (string format)")
+	@Option(name="--send-to", aliases="-s", usage="kafka topic (string format)")
 	private String kafkaTopicFormat = "%s";
 
-	@Option(name="--format", usage="kafka message format (java.lang.String.format)")
+	@Option(name="--format", aliases="-f",usage="kafka message format (java.lang.String.format)")
 	private String kafkaFormat = "%s";
 
-	@Option(name="--filter", usage="message filter regex")
+	@Option(name="--filter", aliases="-F", usage="message filter regex")
 	private String filter = "^";
 	
 	@Option(name="--help", aliases="-h", usage="Show help")
